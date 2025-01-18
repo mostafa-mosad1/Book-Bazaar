0c11b1543ab900afee4d4c388d18b5333b0d6891
mostafa mosad <mostafa-mosad1>
2025-01-18 23:47:07 +0200

    Sign In

--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,4 +1,5 @@
 "use client";
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { SignInFormData } from "@/data";
 import { IFormInputSignIn } from "@/Interface";
 import { useSigninMutation } from "@/redux/features/Api/Authapi";
